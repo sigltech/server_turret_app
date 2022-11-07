634c49ee82c62da2aa57af8ead690b91f057352e
Chris Pucknell <chris.pucknell@gmail.com>
2022-11-07 13:12:20 +0000

    add get_id to questionsController

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -10,6 +10,21 @@ module.exports = {
             }
         })
     },
+    get_id: (req, res) => {
+        const QUESTION_ID = req.params.id
+        Question.findById(QUESTION_ID, (err, question) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send(err)
+            } else if (question === null) {
+                console.log("Result: ", question)
+                res.status(200).send(`No Data: question ${QUESTION_ID} not found`)
+            } else {
+                console.log("Result: ", question)
+                res.status(200).send(question)
+            }
+        })
+    },
     post: (req, res) => {
         const newQuestion = new Question(req.body);
         newQuestion.save();
