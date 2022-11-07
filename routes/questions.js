7a140db6a7eeb00af631cb769e6a7207fdd9d6fb
Chris Pucknell <chris.pucknell@gmail.com>
2022-11-07 13:12:39 +0000

    add /:id route to questions routes, add comments

--- routes/questions.js
+++ routes/questions.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const controller = require('../controllers/questionsController.js')
 
 // routes
-router.get('/', controller.get)
-router.post('/', controller.post)
+router.get('/', controller.get) // Get All
+router.get('/:id', controller.get_id) // Get by id
+router.post('/', controller.post) // Create New Question
 
 module.exports = router
