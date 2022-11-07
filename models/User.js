fdf463cbf558dda7fec687c3db0887108087ff8f
Chris Pucknell <chris.pucknell@gmail.com>
2022-11-07 12:10:49 +0000

    update comment text in user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-//The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural, lowercased version of your model name. Thus, for the example above, the model Tank is for the tanks collection in the database.
+//The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural, lowercased version of your model name i.e. the model Tank is for the tanks collection in the database.
 
 const mongoose = require('mongoose');
 
