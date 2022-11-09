6d2a6ccf9005d0e419d9c1aa216fad4b057f3fbf
Chris Pucknell <chris.pucknell@gmail.com>
2022-11-09 14:15:56 +0000

    update question schema with data validation

--- models/Question.js
+++ models/Question.js
@@ -1,15 +1,45 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    title: String,
-    index: Number,
-    language: String,
-    difficulty: String,
-    description: String,
-    code_question: String,
-    code_answer: String,
-    failed_message: String,
-    success_message: String
+    title: {
+        type: String,
+        require: true
+    },
+    index: {
+        type: Number,
+        min: 0,
+        require: true
+    },
+    language: {
+        type: String,
+        enum: ['javascript', 'python'],
+        require: true
+    },
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium','hard'],
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    code_question: {
+        type: String,
+        require: true
+    },
+    code_answer: {
+        type: String,
+        require: true
+    },
+    failed_message: {
+        type: String,
+        require: true
+    },
+    success_message: {
+        type: String,
+        require: true
+    },
 });
 
 const model = mongoose.model('Question', schema);
